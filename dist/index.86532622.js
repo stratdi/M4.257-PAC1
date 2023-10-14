610113f3e86c394a4456ca25cc84023fb5f49870
Jordi Cruz Lladó <jcl336@MacBook-Air-de-Jordi.local>
2023-10-14 19:46:22 +0200

    Afegida pàgina per temporades

--- dist/index.86532622.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){// Inicialització del plugin feather
-feather.replace()});//# sourceMappingURL=index.86532622.js.map
-
-//# sourceMappingURL=index.86532622.js.map
